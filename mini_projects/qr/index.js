45ceba1499dea431e86c5d81ee470e005e171298
Satyaa <satyasaila18@gmail.com>
2026-09-04 13:00:30 +0530

    mini_projects_added

---
+++ mini_projects/qr/index.js
@@ -0,0 +1,12 @@
+function generateQRcode(){
+    let website=document.getElementById('website').value
+    if(website){
+        let qrcodeContainer=document.getElementById('qrcode')
+        qrcodeContainer.innerHTML=""
+        new QRCode(qrcodeContainer,website)
+        document.getElementById('qrcode-container').style.display='block'
+    }
+    else{
+        alert("please enter the url")
+    }
+}
